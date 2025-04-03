1a42a5ad82e5c7da04fb721ea5ca3cec14c65f99
Adam Lind <adamsplind@gmail.com>
2025-04-03 16:32:24 -0600

    finish nuketracker siteplan

--- nuclear/scripts/weather.js
+++ nuclear/scripts/weather.js
@@ -13,10 +13,63 @@ const currentForecast = document.querySelector('#current-forecast');
 const tomorrowForecast = document.querySelector('#tomorrow-forecast');
 const dayAfterForecast = document.querySelector('#day-after-forecast');
 
+const plantRef = [
+    {reactor: "Fermi", id: 1729},
+    {reactor: "Monticello", id: 1922},
+    {reactor: "Prairie Island", id: 1925},
+    {reactor: "Clinton Power Station", id: 204},
+    {reactor: "Wolf Creek Generating Station", id: 210},
+    {reactor: "PSEG Salem Generating Station", id: 2410},
+    {reactor: "Nine Mile Point Nuclear Station", id: 2589},
+    {reactor: "Peach Bottom", id: 3166},
+    {reactor: "H B Robinson", id: 3251},
+    {reactor: "Oconee", id: 3265},
+    {reactor: "Columbia Generating Station", id: 371},
+    {reactor: "Surry", id: 3806},
+    {reactor: "Point Beach", id: 4046},
+    {reactor: "Waterford 3", id: 4270},
+    {reactor: "Browns Ferry", id: 46},
+    {reactor: "Millstone", id: 566},
+    {reactor: "Donald C Cook", id: 6000},
+    {reactor: "Joseph M Farley", id: 6001},
+    {reactor: "Palo Verde", id: 6008},
+    {reactor: "Calvert Cliffs Nuclear Power Plant", id: 6011},
+    {reactor: "Brunswick", id: 6014},
+    {reactor: "Harris", id: 6015},
+    {reactor: "Perry", id: 6020},
+    {reactor: "Braidwood Generation Station", id: 6022},
+    {reactor: "Byron Generating Station", id: 6023},
+    {reactor: "LaSalle Generating Station", id: 6026},
+    {reactor: "Catawba", id: 6036},
+    {reactor: "McGuire", id: 6038},
+    {reactor: "Beaver Valley", id: 6040},
+    {reactor: "St Lucie", id: 6045},
+    {reactor: "Edwin I Hatch", id: 6051},
+    {reactor: "Grand Gulf", id: 6072},
+    {reactor: "Diablo Canyon", id: 6099},
+    {reactor: "PPL Susquehanna", id: 6103},
+    {reactor: "Limerick", id: 6105},
+    {reactor: "James A Fitzpatrick", id: 6110},
+    {reactor: "Seabrook", id: 6115},
+    {reactor: "PSEG Hope Creek Generating Station", id: 6118},
+    {reactor: "R. E. Ginna Nuclear Power Plant", id: 6122},
+    {reactor: "V C Summer", id: 6127},
+    {reactor: "Comanche Peak", id: 6145},
+    {reactor: "Davis Besse", id: 6149},
+    {reactor: "Sequoyah", id: 6152},
+    {reactor: "Callaway", id: 6153},
+    {reactor: "North Anna", id: 6168},
+    {reactor: "Turkey Point", id: 621},
+    {reactor: "South Texas Project", id: 6251},
+    {reactor: "River Bend Station", id: 6462},
+    {reactor: "Vogtle", id: 649},
+    {reactor: "Watts Bar Nuclear Plant", id: 772}
+];
 
-const currentUrl = 'https://api.eia.gov/v2/seriesid/ELEC.SALES.CO-RES.A?api_key=1VcvnXiJnX84QilrAKjPgXBAOoRew6YkosmA8qou'
+const currentUrl = 'https://api.eia.gov/v2/nuclear-outages/us-nuclear-outages/data/?frequency=daily&data[0]=capacity&data[1]=outage&data[2]=percentOutage&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=25&api_key=1VcvnXiJnX84QilrAKjPgXBAOoRew6YkosmA8qou'
 
 const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=40.23&lon=-111.66&appid=6ef125db37e311a81ba90af94ac76ec1&units=imperial'
+
 
 async function apiFetch(url) {
     try {
@@ -43,23 +96,7 @@ async function apiFetch(url) {
 }
 
 function displayCurrentResults(data) {
-    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-    weatherDesc.innerHTML = `${data.weather[0].main}`
-    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    let desc = data.weather[0].______;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', "something here");
-    high.innerHTML = `${data.main.temp_max}&deg;F`
-    low.innerHTML = `${data.main.temp_min}&deg;F`
-    humidity.innerHTML = `${data.main.humidity}%`
-    let dateObj1 = new Date(data.sys.sunrise * 1000)
-    let utcString1 = dateObj1.toString();
-    let sunriseTime = utcString1.slice(16, 24);
-    sunrise.innerHTML = `${sunriseTime}`
-    let dateObj2 = new Date(data.sys.sunset * 1000)
-    let utcString2 = dateObj2.toString();
-    let sunsetTime = utcString2.slice(16, 24);
-    sunset.innerHTML = `${sunsetTime}`
+
   }
 
 function displayForecastResults(data) {
@@ -76,3 +113,13 @@ function displayForecastResults(data) {
 
 apiFetch(currentUrl);
 // apiFetch(forecastUrl, 2);
+
+
+
+function getObjectByProperty(list, property, value) {
+    return list.find(obj => obj[property] === value);
+  }
+
+const obj1 = getObjectByProperty(plantRef, "reactor", "River Bend Station")
+console.log(obj1)
+
